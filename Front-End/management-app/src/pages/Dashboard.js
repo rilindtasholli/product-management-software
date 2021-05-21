4ac64aef18a9ecf7614e49b96b4ded232057b513
rilindtasholli <rilindtasholli@gmail.com>
2021-05-21 21:34:29 +0100

    created Login & Register Forms

--- Front-End/management-app/src/pages/Dashboard.js
+++ Front-End/management-app/src/pages/Dashboard.js
@@ -5,23 +5,31 @@ export class Dashboard extends Component {
     render() {
         return (
             <div className='main-content'>
-                <div className='sales-activity'>
-                    <h1 className='container-title' >Sales Activity</h1>
-                    <div className='sales-items'>
-                        <div className='sales-item'>
-                            <h1 className='item-data'>15</h1>
-                            <h3 className='item-title'>Today</h3>
+                <div>
+                    <div className='sales-activity'>
+                        <h1 className='container-title' >Sales Activity</h1>
+                        <div className='sales-items'>
+                            <div className='sales-item'>
+                                <h1 className='item-data'>15</h1>
+                                <h3 className='item-title'>Today</h3>
+                            </div>
+                            <div className='sales-item'>
+                                <h1 className='item-data'>152</h1>
+                                <h3 className='item-title'>This Month</h3>
+                            </div>
+                            <div className='sales-item'>
+                            <h1 className='item-data'>1860</h1>
+                                <h3 className='item-title'>This Year</h3>
+                            </div>
                         </div>
-                        <div className='sales-item'>
-                            <h1 className='item-data'>152</h1>
-                            <h3 className='item-title'>This Month</h3>
-                        </div>
-                        <div className='sales-item'>
-                        <h1 className='item-data'>1860</h1>
-                            <h3 className='item-title'>This Year</h3>
-                        </div>
-                    </div>
-                </div>    
+                    </div>    
+                </div>
+                {/* <div className='account-data'>
+                    
+                </div> */}
+
+
+                
             </div>
         )
     }
