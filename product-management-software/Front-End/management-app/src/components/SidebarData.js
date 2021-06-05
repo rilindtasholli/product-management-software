caaafce812ac33fd54d39ed96ba8d1824c5b45ae
blenda <blendamolliqaj@gmail.com>
2021-06-05 20:28:19 +0200

    suppliers

---
+++ product-management-software/Front-End/management-app/src/components/SidebarData.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { AiFillDashboard } from 'react-icons/ai';
+import { FiPackage } from 'react-icons/fi';
+import { AiFillAppstore } from 'react-icons/ai';
+import { FaChartLine } from 'react-icons/fa';
+import { FaWarehouse } from 'react-icons/fa';
+import {BsFillPersonFill} from 'react-icons/bs'
+import {FaPersonBooth} from 'react-icons/fa'
+import { FaLuggageCart } from 'react-icons/fa';
+import { GiPieChart } from 'react-icons/gi';
+import { RiSettings4Fill } from 'react-icons/ri';
+import { RiAccountCircleFill } from 'react-icons/ri';
+
+export const SidebarData = [
+    {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <AiFillDashboard className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Products',
+        path: '/products',
+        icon: <FiPackage className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Categories',
+        path: '/categories',
+        icon: <AiFillAppstore className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Sales',
+        path: '/sales',
+        icon: <FaChartLine className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Employees',
+        path: '/employees',
+        icon: <BsFillPersonFill className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Clients',
+        path: '/clients',
+        icon: <FaPersonBooth className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Suppliers',
+        path: '/suppliers',
+        icon: <FaWarehouse className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Orders',
+        path: '/orders',
+        icon: <FaLuggageCart className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Data & Reports',
+        path: '/reports',
+        icon: <GiPieChart className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: <RiSettings4Fill className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Account',
+        path: '/account',
+        icon: <RiAccountCircleFill className='nav-link-icon'/>,
+        cName: 'nav-text'
+    },
+]
---
+++ product-management-software/Front-End/management-app/src/pages/Clients.js
@@ -0,0 +1,235 @@
+import React, { Component } from "react";
+import { Table, Button, ButtonToolbar } from "react-bootstrap";
+import { AddClient } from "./AddClient";
+import "./css/Clients.css";
+import { EditClient } from "./EditClient";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export class Clients extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { addModalShow: false, editModalShow: false };
+  }
+
+  render() {
+    let addModalClose = () => this.setState({ addModalShow: false });
+    let editModalClose = () => this.setState({ editModalShow: false });
+    return (
+      <div className="main-content-client">
+        <Table className=" w-75 m-auto  font-weight-bold" striped  size="md">
+          <thead
+            style={{
+              background: "#002e58",
+              color: "white",
+              fontWeight: "bold",
+            
+            }}
+          >
+            <tr>
+              <th>ClientId</th>
+              <th>ClientName</th>
+              <th>ClientAddress</th>
+              <th>PhoneNumber</th>
+              <th style={{ width: 250 }}>Options</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Besart Ibishi</td>
+              <td>Gilan</td>
+              <td>044-111-111</td>
+              <td>
+                <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60 }}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>2</td>
+              <td>Rilind Tasholli</td>
+              <td>Lipjan</td>
+              <td>044-222-222</td>
+              <td>
+              <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60}}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+                    
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>3</td>
+              <td>Gresa Shala</td>
+              <td>Prizren</td>
+              <td>044-333-333</td>
+              <td>
+                {" "}
+                <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60 }}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+                    
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+
+            
+            <tr>
+              <td>4</td>
+              <td>Ernise Sallahu</td>
+              <td>Gjakove</td>
+              <td>044-444-444</td>
+              <td>
+                {" "}
+                <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60 }}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+                    
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+
+            
+            <tr>
+              <td>5</td>
+              <td>Blenda Molliqaj</td>
+              <td>Deçan</td>
+              <td>044-555-555</td>
+              <td>
+                {" "}
+                <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60 }}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+                    
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+
+            
+            <tr>
+              <td>6</td>
+              <td>Lani Zymberi</td>
+              <td>Drenas</td>
+              <td>044-666-666</td>
+              <td>
+                {" "}
+                <ButtonToolbar>
+                  <Button
+                    style={{ background: "#035bad", width: 60 }}
+                    className="mr-2"
+                    onClick={() => this.setState({ editModalShow: true })}>
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ width: 80 }}
+                    variant="danger"
+                    
+                  >
+                    Delete
+                  </Button>
+                </ButtonToolbar>
+                <EditClient
+                  show={this.state.editModalShow}
+                  onHide={editModalClose}
+                />
+              </td>
+            </tr>
+            
+
+          </tbody>
+          <ButtonToolbar className="add-button">
+          <Button
+            style={{background:'#035bad'}}
+            onClick={() => this.setState({ addModalShow: true })}
+          >
+            Add Client
+          </Button>
+          <AddClient
+            show={this.state.addModalShow}
+            onHide={addModalClose}
+          ></AddClient>
+        </ButtonToolbar>
+        </Table>
+
+    
+      </div>
+    );
+  }
+}
+
+export default Clients;
