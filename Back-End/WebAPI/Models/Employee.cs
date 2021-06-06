687671abd41e1df20df07d1e6e772ed59a68becf
Gresa Shala <gressaash@gmail.com>
2021-06-06 17:35:52 +0200

    added Employee model and controller

---
+++ Back-End/WebAPI/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ManagementAPI.Models
+{
+    public class Employee
+    {
+        public int Emp_id { get; set; }
+
+        public string Emp_name { get; set; }
+
+        public string Emp_phone { get; set; }
+
+        public string Emp_email { get; set; }
+
+        public decimal Emp_hourly_wage { get; set; }
+
+        public decimal Emp_hours { get; set; }
+    
+    }
+}
